f983ca53a4330366dbde7a7d4fee15826b6583de
Muhammad Hamza <MuhammadHamza@DESKTOP-E0SVIVJ>
2023-12-04 23:45:28 +0500

    FrontEnd Completed and CRUD Operations

--- server/routers/userroute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usercontroller = require('../Controller/UserController');
-
-router.post('/register',usercontroller.register);
-
-module.exports = router;
